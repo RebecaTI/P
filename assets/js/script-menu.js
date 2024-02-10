936cb522ce814ea966350c6a62f223f194bd1714
Rebeca Oliveira <mebuffbeka@gmail.com>
2024-02-10 16:14:21 -0300

    organizando portfolio

--- assets/js/script-menu.js
+++ assets/js/script-menu.js
@@ -1,5 +1,5 @@
 
-window.addEventListener('scroll', function () {
+window.addEventListener("scroll", function () {
     let header = document.querySelector('#header');
     header.classList.toggle('rolagem', window.scrollY > 200);
 })
