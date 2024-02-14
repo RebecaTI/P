e6c1d1dbdc879625e254dfb548c811d5842662df
Rebeca Oliveira <mebuffbeka@gmail.com>
2024-02-14 05:19:35 -0300

    Atualizando os arquivos do site

--- assets/js/script-menu.js
+++ assets/js/script-menu.js
@@ -1,5 +1,5 @@
 
 window.addEventListener("scroll", function () {
     let header = document.querySelector('#header');
-    header.classList.toggle('rolagem', window.scrollY > 200);
+    header.classList.toggle('rolagem', window.scrollY > 100);
 })
