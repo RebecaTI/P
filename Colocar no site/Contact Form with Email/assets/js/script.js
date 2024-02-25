c787eb4e094a177d770b530ff9ed52190c3bcdeb
Rebeca Oliveira <mebuffbeka@gmail.com>
2024-02-25 16:32:58 -0300

    testando tamanho da fonte

--- Colocar no site/Contact Form with Email/assets/js/script.js
+++ Colocar no site/Contact Form with Email/assets/js/script.js
@@ -9,6 +9,7 @@ function sendEmail() {
     const bodyMessage = `Full Name : ${fullName.value}<br> Email : ${email.value}<br> Phone Number : ${phone.value}<br> Subject : ${subject.value}<br> Message : ${mess.value}<br>`
 
     Email.send({
+        SecureToken : "1fbfa3db-44e1-4750-9402-fd44ef648116",
         Host : "smtp.elasticemail.com",
         Username : "rebecaoliveirafullstack@gmail.com",
         Password : "1717B35DBD223C2A0437A3906FC93AE14BFB",
@@ -38,6 +39,14 @@ function checkInputs() {
             item.parentElement.classList.add("error");
         }
 
+        if(items[1].value != ""){
+            checkEmail();
+        }
+
+        items[1].addEventListener("keyup", () => {
+            checkEmail();
+        });
+
         item.addEventListener("keyup", () =>{
             if(item.value != ""){
                 item.classList.remove("error");
@@ -53,10 +62,19 @@ function checkInputs() {
 
 function checkEmail() {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const errorTxtEmail = document.querySelector(".error-txt.email");
+
 
     if(!email.value.match(emailRegex)){
         email.classList.add("error");
         email.parentElement.classList.add("error");
+
+        if (email.value != ""){
+            errorTxtEmail.innerText = "Enter a valid email address";
+        }
+        else {
+            errorTxtEmail.innerText = "Email Adress can't be blank";
+        }
     }
     else{
         email.classList.remove("error");
@@ -64,10 +82,15 @@ function checkEmail() {
     }
 }
 
-
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-
     checkInputs()
-    // sendEmail();
+    
+    if(!fullName.classList.contains("error") && !email.classList.contains("error") && !phone.classList.contains("error") && !subject.classList.contains("error") && !mess.classList.contains("error")) {
+     sendEmail();
+
+     form.reset();
+     return false;
+    }
+
 })
